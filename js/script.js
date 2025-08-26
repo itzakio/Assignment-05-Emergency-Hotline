9f82b4ac808f27b20b383586e15c6d9fa6177799
itzakio <itzakio3@gmail.com>
2025-08-27 01:15:00 +0600

    copy functionality added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ document.getElementById("all-cards")
         getElement("heart-count").innerText = newCount;
     }
 })
+
+// copy button functionality
+document.getElementById("all-cards")
+.addEventListener("click", function(e){
+    const copyBtn = e.target.className.includes("copy-btn")
+    if(copyBtn === true){
+        
+        const copyCount = Number(getElement("copy-count").innerText);
+        const copyText = getElement("copy-btn-id").parentNode.parentNode.childNodes[7].innerText
+        navigator.clipboard.writeText(copyText);
+        const newCopyCount = copyCount + 1;
+        getElement("copy-count").innerText = newCopyCount;
+        alert(`Contact Number copied ${copyText}`);
+    }
+})
