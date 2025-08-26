b17b3de048a1fcb558d85a31f474a88bf5dc4d49
itzakio <itzakio3@gmail.com>
2025-08-27 02:27:16 +0600

    calling functionality added, alart on call and cut coin and alart when coin is less then 20

--- js/script.js
+++ js/script.js
@@ -29,3 +29,48 @@ document.getElementById("all-cards")
         alert(`Contact Number copied ${copyText}`);
     }
 })
+
+// call button functionality
+document.getElementById("all-cards")
+.addEventListener("click", function(e){
+    const cardImg = e.target.parentNode.parentNode.childNodes[1].childNodes[1].childNodes[1].src;
+    const cardTitle = e.target.parentNode.parentNode.childNodes[3].innerText;
+    const cardContact = e.target.parentNode.parentNode.childNodes[7].innerText;
+    const time = new Date().toLocaleTimeString()
+    const historyContainer = getElement("call-history-container")
+
+    const callBtn = e.target.className.includes("call-btn")
+    if(callBtn === true){
+        const coins = Number(getElement("coins").innerText);
+        if (coins < 20){
+            alert("You do not have enough coins to call")
+        }
+        else{
+            // calling functionality 
+            const newCoinCount = coins - 20;
+            getElement("coins").innerText = newCoinCount;
+            alert(`📞 Calling ${cardTitle} - ${cardContact}`)
+            
+            // history functionality
+            
+            const div = document.createElement("div");
+            div.innerHTML = `
+                 <div class="p-4 rounded-lg bg-gray-100">
+                    <div class="flex justify-between">
+                        <h3 class="text-lg font-semibold">${cardTitle}</h3>
+                        <div class="size-8 bg-red-100 flex justify-center items-center rounded-md">
+                            <img class="size-4 " src="${cardImg}" alt="">
+                        </div>
+                    </div>
+                    <div class="flex justify-between">
+                        <p class="text-gray-500">${cardContact}</p>
+                        <p>${time}</p>
+                    </div>
+                </div>
+            `
+            historyContainer.appendChild(div);
+            console.log(div.innerHTML);
+        }
+        
+    }
+})
